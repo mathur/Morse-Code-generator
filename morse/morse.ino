a77f82f503d040f02d256f6bcd774e3e1fe00ec2
Stephen Pinkerton <stephenmpinkerton@gmail.com>
2014-09-17 16:37:15 -0500

    Lucian's changes

--- morse/morse.ino
+++ morse/morse.ino
@@ -1,6 +1,7 @@
 /*
 25 element array of morse symbols - cast characters to ints and look them up
 41 - 5A (base 16) or 65 - 90 (base 10) for the capital letters
+http://www.asciitable.com/
 */
 
 /* TODO:
@@ -10,28 +11,64 @@
 
 int led = 13;
 
-char alphabet[26] = {
-  '.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', 
-  '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', 
-  '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', 
-  '-.--', '--..'
+String alphabet[] = {
+  ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", 
+  "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", 
+  "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", 
+  "-.--", "--.."
 };
 
 void setup() {
+  Serial.begin(9600);
   pinMode(led, OUTPUT);
-  for (int i = 0; i < sizeof(alphabet); i ++) {
+  for (int i = 0; i < 26; i++) {
+    Serial.print("alphabet[");
+    Serial.print(i);
+    Serial.print("] = ");
     Serial.print(alphabet[i]);
+    Serial.print("\n");
+  }
+  
+  char letter = 'l';
+  
+  switch(letter) {
+    case 'a': Serial.print(alphabet[0]);
+    case 'b': Serial.print(alphabet[1]);
+    case 'c': Serial.print(alphabet[2]);
+    case 'd': Serial.print(alphabet[3]);
+    case 'e': Serial.print(alphabet[4]);
+    case 'f': Serial.print(alphabet[5]);
+    case 'g': Serial.print(alphabet[6]);
+    case 'h': Serial.print(alphabet[7]);
+    case 'i': Serial.print(alphabet[8]);
+    case 'j': Serial.print(alphabet[9]);
+    case 'k': Serial.print(alphabet[10]);
+    case 'l': Serial.print(alphabet[11]);
+    case 'm': Serial.print(alphabet[12]);
+    case 'n': Serial.print(alphabet[13]);
+    case 'o': Serial.print(alphabet[14]);
+    case 'p': Serial.print(alphabet[15]);
+    case 'q': Serial.print(alphabet[16]);
+    case 'r': Serial.print(alphabet[17]);
+    case 's': Serial.print(alphabet[18]);
+    case 't': Serial.print(alphabet[19]);
+    case 'u': Serial.print(alphabet[20]);
+    case 'v': Serial.print(alphabet[21]);
+    case 'w': Serial.print(alphabet[22]);
+    case 'x': Serial.print(alphabet[23]);
+    case 'y': Serial.print(alphabet[24]);
+    case 'z': Serial.print(alphabet[25]);
   }
 }
 
 void loop() {
   digitalWrite(led, HIGH);
-  delay(1000);
+  delay(200);
   digitalWrite(led, LOW);
-  delay(1000);
+  delay(200);
 }
 
-void parseMorse(char[] morse) {
+/*void parseMorse(char[] morse) {
   
-}
+}*/
 
